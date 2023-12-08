5f3669651f37e0e7927ff13d7b7b4d281b48f6cd
Nitr0Skay <cin.miko94@gmail.com>
2023-12-09 00:25:33 +0100

    Add a simple model

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
+require "model.php";
+
+$model = new Model;
+
+$products = $model->getData();
+
 ?>
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <title> Products </title>
+</head>
+<body>
+    <header class="header" id="header"> 
+        <a href="#" title="Nagłówek Strony"> 
+            <h1 class="page-title" id="page-title"> Products </h1>
+        </a>
+    </header>
+
+    <?php foreach ($products as $product): ?>
+        <h2> <?= htmlspecialchars($product["name"]) ?> </h2>
+        <p> <?= htmlspecialchars($product["description"]) ?></p>
+    <?php endforeach; ?>
+</body>
+</html>
