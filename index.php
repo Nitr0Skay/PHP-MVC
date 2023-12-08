2eb1cb1776ccf5753fc8962f971b76e2fea119ae
Nitr0Skay <cin.miko94@gmail.com>
2023-12-09 00:28:40 +0100

    Add a simple view

--- index.php
+++ index.php
@@ -6,23 +6,6 @@ $model = new Model;
 
 $products = $model->getData();
 
-?>
-<!DOCTYPE html>
-<html lang="pl" dir="ltr">
-<head>
-    <meta charset="UTF-8" />
-    <title> Products </title>
-</head>
-<body>
-    <header class="header" id="header"> 
-        <a href="#" title="Nagłówek Strony"> 
-            <h1 class="page-title" id="page-title"> Products </h1>
-        </a>
-    </header>
+require "view.php";
 
-    <?php foreach ($products as $product): ?>
-        <h2> <?= htmlspecialchars($product["name"]) ?> </h2>
-        <p> <?= htmlspecialchars($product["description"]) ?></p>
-    <?php endforeach; ?>
-</body>
-</html>
+?>
